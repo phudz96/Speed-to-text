3569522cfa53ec7a50a1fb8b20ab79c211958046
phudz96 <93246897+phudz96@users.noreply.github.com>
2021-10-27 15:33:46 +0700

    sửa lại tí

--- speech-to-text.java
+++ speech-to-text.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }else
             Toast.makeText(this, "Voice N/A", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Khó hiểu", Toast.LENGTH_SHORT).show();
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
